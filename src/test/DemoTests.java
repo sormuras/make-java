eb0a068c8d252fbc7ca26d645786174961490f07
Christian Stein <sormuras@gmail.com>
2019-02-16 07:35:58 +0100

    Copy demo project into temporary workspace

--- src/test/DemoTests.java
+++ src/test/DemoTests.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
@@ -6,21 +7,36 @@ import java.nio.file.Path;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class DemoTests {
   private final CollectingLogger logger = new CollectingLogger("*");
+  private final Make make = new Make(logger, Path.of("."), List.of());
 
   @Nested
   class JigsawQuickStart {
 
     @Test
-    void greetings() {
-      var base = Path.of("demo", "jigsaw-quick-start", "greetings");
+    void greetings(@TempDir Path workspace) {
+      var demo = Path.of("demo", "jigsaw-quick-start", "greetings");
+      var base = workspace.resolve(demo.getFileName());
+      make.run(new Make.Action.TreeCopy(demo, base));
+
+      var logger = new CollectingLogger("*");
       var make = new Make(logger, base, List.of());
       assertEquals(base, make.base);
       assertTrue(Files.isDirectory(make.based("src")));
       assertEquals("greetings", make.project.name);
       assertEquals("1.0.0-SNAPSHOT", make.project.version);
+      assertEquals(0, make.run());
+      assertLinesMatch(
+          List.of(
+              "Running action Banner...",
+              "Make.java - " + Make.VERSION,
+              "Action Banner succeeded.",
+              "Running action Check...",
+              "Action Check succeeded."),
+          logger.getLines());
     }
   }
 }
