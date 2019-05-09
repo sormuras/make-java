cda4fa25a4ed3e86d30975a281e5e7e67095333e
Christian Stein <sormuras@gmail.com>
2019-05-09 04:41:48 +0200

    Add https://github.com/sormuras/modular-world integration tests

--- src/test/ModularWorldTests.java
+++ src/test/ModularWorldTests.java
@@ -1,35 +1,66 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.io.TempDir;
 
 class ModularWorldTests {
 
-  @Test
-  void test(@TempDir Path temp) throws Exception {
+  /** https://github.com/sormuras/modular-world/releases */
+  private static final String VERSION = "0.1";
+
+  @TestFactory
+  Stream<DynamicTest> buildModularWorld(@TempDir Path temp) throws Exception {
     // download and extract
-    var uri = URI.create("https://github.com/sormuras/modular-world/archive/master.zip");
-    var zip = "modular-world-master.zip";
-    download(line -> {}, temp.resolve(zip), uri);
-    new ProcessBuilder("jar", "--extract", "--file", zip)
-        .directory(temp.toFile())
-        .start()
-        .waitFor();
+    var uri = URI.create("https://github.com/sormuras/modular-world/archive/" + VERSION + ".zip");
+    var zip = "modular-world-" + VERSION + ".zip";
+    download(log -> {}, temp.resolve(zip), uri);
+    var extract =
+        new ProcessBuilder("jar", "--extract", "--file", zip).directory(temp.toFile()).start();
+    assertEquals(0, extract.waitFor(), extract.toString());
+    var homes = Make.Util.listDirectories(temp.resolve("modular-world-" + VERSION));
+    assertEquals(3, homes.size());
+    // build all modular projects
+    return homes.stream()
+        .map(home -> dynamicTest(home.getFileName().toString(), () -> build(home)));
+  }
+
+  private void build(Path home) throws Exception {
+    var logger = new CollectingLogger("*");
+    var project = home.getFileName().toString();
+    var version = "0-TEST";
+    var work = home.resolve("work");
+    Files.createDirectories(work);
+    var main = new Make.Realm("main", Path.of("src", "main"));
+
+    var make = new Make(logger, false, project, version, home, work, List.of(main));
+    assertEquals(
+        0,
+        make.run(System.out, System.err),
+        logger.toString() + "\n\ntree\n" + String.join("\n|  ", treeWalk(home)));
     assertLinesMatch(
-        List.of("000-a", "001-abc", "010-mr-a"),
-        Make.Util.listDirectoryNames(temp.resolve("modular-world-master")));
-    // build and run each
-    // compare expected to actual results
+        List.of(
+            "Make.java - " + Make.VERSION,
+            "  args = []",
+            String.format("Building %s %s", project, version),
+            "  home = " + home.toUri(),
+            "  work = " + work.toUri(),
+            "  realms[0] = Realm{name=main, source=" + Path.of("src/main") + "}",
+            ">> BUILD >>",
+            "Build successful after \\d+ ms\\."),
+        logger.getLines());
+    // treeWalk(home).forEach(System.out::println);
   }
 
   static Path download(Consumer<String> logger, Path target, URI uri) throws Exception {
@@ -63,5 +94,26 @@ class ModularWorldTests {
       logger.accept(String.format("Downloaded %s successfully.", fileName));
     }
     return target;
+  }
+
+  /** Walk directory tree structure. */
+  static List<String> treeWalk(Path root) {
+    var lines = new ArrayList<String>();
+    treeWalk(root, lines::add);
+    return lines;
+  }
+
+  /** Walk directory tree structure. */
+  static void treeWalk(Path root, Consumer<String> out) {
+    try (var stream = Files.walk(root)) {
+      stream
+          .map(root::relativize)
+          .map(path -> path.toString().replace('\\', '/'))
+          .sorted()
+          .filter(Predicate.not(String::isEmpty))
+          .forEach(out);
+    } catch (Exception e) {
+      throw new Error("Walking tree failed: " + root, e);
+    }
   }
 }
