8db743ef5b48c67216c06fa739d8e76fb0189583
Christian Stein <sormuras@gmail.com>
2019-05-17 18:45:56 +0200

    Introduce package-private field in main demo class

--- demo/jigsaw-quick-start/greetings-world-with-main-and-test/src/main/org.astro/org/astro/World.java
+++ demo/jigsaw-quick-start/greetings-world-with-main-and-test/src/main/org.astro/org/astro/World.java
@@ -2,7 +2,9 @@ package org.astro;
 
 public class World {
 
+  static final String PACKAGE_PRIVATE_FIELD = "world";
+
   public static String name() {
-    return "world";
+    return PACKAGE_PRIVATE_FIELD;
   }
 }
