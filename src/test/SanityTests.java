e50f26f6999cdf7d2e505dd066cb67d612c76e68
Christian Stein <sormuras@gmail.com>
2019-05-21 17:11:38 +0200

    Assert normal and error output expectations

--- src/test/SanityTests.java
+++ src/test/SanityTests.java
@@ -1,6 +1,14 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class SanityTests {
@@ -19,6 +27,10 @@ class SanityTests {
     process.waitFor(9, TimeUnit.SECONDS);
     var code = process.exitValue();
     assertEquals(1, code);
+    assertStreams(
+        List.of("Making Make.java master..."),
+        List.of("No module found: " + Make.USER_PATH),
+        process);
   }
 
   @Test
@@ -30,5 +42,22 @@ class SanityTests {
     process.waitFor(9, TimeUnit.SECONDS);
     var code = process.exitValue();
     assertEquals(1, code);
+    assertStreams(
+        List.of("Making Make.java master..."),
+        List.of("No module found: " + Make.USER_PATH, "Make.java failed with error code: 1"),
+        process);
+  }
+
+  static void assertStreams(List<String> expectedOut, List<String> expectedErr, Process process) {
+    assertLinesMatch(expectedOut, lines(process.getInputStream()));
+    assertLinesMatch(expectedErr, lines(process.getErrorStream()));
+  }
+
+  static List<String> lines(InputStream stream) {
+    try (var reader = new BufferedReader(new InputStreamReader(stream))) {
+      return reader.lines().collect(Collectors.toList());
+    } catch (IOException e) {
+      throw new UncheckedIOException("Reading from stream failed!", e);
+    }
   }
 }
