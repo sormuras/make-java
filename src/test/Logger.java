b16f0d804f01b6a33a66f733977ff2d369728e09
Christian Stein <sormuras@gmail.com>
2020-01-20 09:04:46 +0100

    Support verbose "print all messages" mode

--- src/test/Logger.java
+++ src/test/Logger.java
@@ -1,8 +1,20 @@
 import java.lang.System.Logger.Level;
 
 public class Logger implements Make.Logger {
+
+  boolean verbose;
+
+  Logger() {
+    this(false);
+  }
+
+  Logger(boolean verbose) {
+    this.verbose = verbose;
+  }
+
   @Override
   public Make.Logger log(Level level, String format, Object... args) {
+    if (verbose) System.out.println(String.format(format, args));
     return this;
   }
 }
